449d64199eadfe0d3a161d7083bf637d092080e2
Tyler Gaw <tagaw@ucsc.edu>
2025-12-10 15:03:15 -0800

    setup for github page deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://tagaw.github.io",
   vite: {
     plugins: [tailwindcss()]
   }

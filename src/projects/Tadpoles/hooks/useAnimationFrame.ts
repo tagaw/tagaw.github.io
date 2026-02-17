5c1af52e62e0f54e6dbfe7b070bc2efcf641b100
Tyler Gaw <tagaw@ucsc.edu>
2026-02-17 09:19:13 -0800

    added missing dependecy array to hook

--- src/projects/Tadpoles/hooks/useAnimationFrame.ts
+++ src/projects/Tadpoles/hooks/useAnimationFrame.ts
@@ -1,17 +1,22 @@
 import { useRef, useEffect } from "react";
 
-export default function useAnimationFrame( tick: () => void, deps: any[] = []) {
+export default function useAnimationFrame( tick: () => void, deps: any[] | null = null) {
     const frameID = useRef<number | null>(null);
     const callback = () => {
         tick();
         frameID.current = requestAnimationFrame(callback);
     }
-    useEffect( () => {
+    const effect = () => {
         frameID.current = requestAnimationFrame(callback);
         return () => {
             if (frameID.current)
                 cancelAnimationFrame(frameID.current);
             // console.log("cleaned up animation frame with id: ", frameID.current);
         }
-    });
+    }
+    if (deps === null) {
+        useEffect(effect);
+    } else {
+        useEffect(effect, deps);
+    }
 }

529e34bf99f154d87363c69b18d5c0ff7dcfcbe3
Tyler Gaw <tagaw@ucsc.edu>
2026-02-17 09:20:24 -0800

    start handling food interactions

--- src/projects/Tadpoles/TadpoleApp.tsx
+++ src/projects/Tadpoles/TadpoleApp.tsx
@@ -2,18 +2,27 @@ import { StrictMode, useState } from "react";
 import Pond from "./react/Pond";
 
 export default function App() {
-    const [tadpoleCount, setTadpoleCount] = useState<number>(0);
-
+    const [tadpoleCount, setTadpoleCount] = useState<number>(1);
+    const [foodAmt, setFoodAmt] = useState<number>(tadpoleCount+1);
+    const [maxFoodAmt, setMaxFoodAmt] = useState<number>(tadpoleCount+1);
     
+    function handleFoodChange(change: 1 | -1) {
+        setFoodAmt(c => c + change);
+    }
+
+    function setMaxFood(amount: number) {
+        setMaxFoodAmt(amount);
+        setFoodAmt(amount);
+    }
 
     return (
         <StrictMode>
-        <div className="">Tapoles:{tadpoleCount} <span className="font-bold border" onClick={() => setTadpoleCount(c => c+1)}>
+        <div className="">Tapoles:{tadpoleCount} <span className="font-bold border" onClick={() => {setTadpoleCount(c => c+1); setMaxFood(tadpoleCount+1+1)}}>
             +
-        </span>
+        </span>  | Food: {foodAmt}
         </div>
         <div className="w-full grow bg-white max-w-7xl">
-            <Pond tadpoleCt={tadpoleCount}/>
+            <Pond foodCt={foodAmt} tadpoleCt={tadpoleCount} handleFoodChange={handleFoodChange} key={tadpoleCount}/>
         </div>
         </StrictMode>
     );
--- src/projects/Tadpoles/react/Pond.tsx
+++ src/projects/Tadpoles/react/Pond.tsx
@@ -1,23 +1,93 @@
 import { useEffect, useRef, useState } from "react";
 import Tadpole from "./Tadpole";
+import useAnimationFrame from "../hooks/useAnimationFrame";
+import { perf } from "astro/runtime/client/dev-toolbar/apps/audit/rules/perf.js";
 
 
 export type props = { 
     tadpoleCt: number;
+    foodCt: number;
+    handleFoodChange: (change: 1 | -1) => void;
 };
 
-export default function Pond( { tadpoleCt }: props) {
+export default function Pond( { tadpoleCt, foodCt, handleFoodChange }: props) {
+    const foodResourceCt = 10;
+    const foodRadius = 10//0.3*Math.sqrt(foodResourceCt);
+
     const [x, setX] = useState<number>(1);
     const svgRef = useRef<SVGSVGElement>(null);
+    const [speeds,setSpeeds] = useState<number[]>(new Array(tadpoleCt).fill(0).map(() => Math.max(1, Math.random()+0.5)));
     
+    const foodRefs = useRef<(SVGCircleElement | null)[]>(new Array(foodCt).fill(null));
+    const [foodPos, setFoodPos] = useState<{x: number, y: number, amt: number}[]>(new Array(foodCt).fill({x: -100, y: -100, amt: foodResourceCt}));
+    const freeFood = useRef<number[]>(new Array(foodCt).fill(0).map((_, i) => i));
+    const usedFood = useRef<number[]>([]);
+
+    useEffect(() => {
+        // TODO: decide between adding tadpoles or creating new pond
+        // reinitialize speeds when pond changes
+        setSpeeds(new Array(tadpoleCt).fill(0).map(() => Math.max(1, Math.random()+0.5)));
+    }, [tadpoleCt])
+    // console.log(freeFood.current, usedFood.current);
+
+    let frameId = 1;
     
+
+    function handleFoodDrop(e: React.MouseEvent) {
+        let start = 0;
+        const rect = svgRef.current?.getBoundingClientRect();
+        const x = e.clientX - (rect?.left || 0);
+        const y = e.clientY - (rect?.top || 0);
+        if (foodCt > 0) {
+            const foodIndex = freeFood.current.pop()!;
+            usedFood.current.push(foodIndex);
+            const food = foodRefs.current[foodIndex]!;
+
+            handleFoodChange(-1);
+            console.log(foodCt, freeFood.current, usedFood.current);
+            food.setAttribute('r', foodRadius.toString());
+            setFoodPos(prev => {
+                const newArr = [...prev];
+                newArr[foodIndex] = {x, y, amt: foodResourceCt};
+                return newArr;
+            });
+            const tick = () => {
+                const start = performance.now();
+                const t = () => {
+                    const now = performance.now();
+                    const elapsed = now - start;
+                    if (elapsed < 3000) {
+                        frameId = requestAnimationFrame(t);
+                    } else {
+                        console.log("removing food");
+                        food.setAttribute('r', '0');
+                        freeFood.current.push(foodIndex);
+                        usedFood.current.shift();
+                        handleFoodChange(1);
+                    }
+                }
+                t();
+            }
+            tick();
+        }
+    }
+
+    useEffect(() => {
+        return () => {
+            console.log('clean up')
+            frameId && cancelAnimationFrame(frameId);
+        }}, [tadpoleCt]);
     return (
         <>
         <div className='w-full h-full border-4 flex-none'>
-            <svg ref={svgRef!} className="w-full h-full" onClick={() => setX(val => (val|| 0) + 1)}>
-                 {/* <text x="20" y="35" className="small">{x || 'null'}</text> */}
-                 {new Array(tadpoleCt).fill(0).map((_,i) => 
-                    <Tadpole canvasRef={svgRef} speed={1} key={`t${tadpoleCt}c${i}`} />)}
+            <svg ref={svgRef!} className="w-full h-full" onClick={handleFoodDrop}>
+                {foodPos.map((pos, i) => 
+                    <circle ref={el => {foodRefs.current[i] = el}} cx={pos.x} cy={pos.y} r={0} fill="green" key={i}/>)}
+                {speeds.map((speed, i) => 
+                    <Tadpole canvasRef={svgRef} speed={speed} key={`t${tadpoleCt}c${i}`} />)}   
+                {/*                 
+                {speeds.map((speed, i) => 
+                    <Tadpole canvasRef={svgRef} speed={speed} key={i} />)}    */}
             </svg>
         </div>      
         </>
